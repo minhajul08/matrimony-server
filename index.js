ba528a3f7ae8e9ff933ea6c7a1c9e310927c878e
minhajul08 <minhazmehadi61@gmail.com>
2024-11-05 23:43:26 +0600

    Add endpoint to delete favorite item by ID

--- index.js
+++ index.js
@@ -67,6 +67,13 @@ async function run() {
         const result = await addToFavorite.find(query).toArray();
         res.send(result);
     });
+
+    app.delete ('/favorite/:id', async (req,res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await addToFavorite.deleteOne(query);
+      res.send (result)
+    })
     
      
 
